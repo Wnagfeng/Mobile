74b01637467649e09d09fe4ae044c67adf7ea313
CoderJoon <14905029+stained-with-dust@user.noreply.gitee.com>
2024-09-19 22:32:10 +0800

    搭建基本页面 :sparkles:

---
+++ src/views/classify/productClassify.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Product Classify</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
